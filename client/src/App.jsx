fc2a6549adb444a0a7f533896355e6a4eb403767
masonnguyen1243 <meowjcisgod@gmail.com>
2025-04-20 16:28:23 +0700

    Create client

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+const App = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+export default App;
